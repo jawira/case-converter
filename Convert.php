9c7f9c26dd4fcc222b651d2dcc956c770e5cd1f9
jawira <jawitugal@gmail.com>
2015-12-02 15:54:20 +0100

    camel and snake methods

--- Convert.php
+++ Convert.php
@@ -43,6 +43,37 @@ class Convert
         return (mb_strpos($str, '_') !== false) ? self::SNAKE : self::CAMEL;
     }
 
+    /**
+     * Returns an CamelCase string
+     *
+     * @param bool|false $upper If true the result will be Pascal Case.
+     * @param bool|false $private If true the result will start with an underscore
+     * @return string
+     */
+    public function camel($upper = false, $private = false)
+    {
+        $copy = array_map(function ($w) {
+            return ucfirst(mb_strtolower($w));
+        }, $this->words);
+        $str = implode('', $copy);
+
+        return $private ? '_' : '' . $upper ? ucfirst($str) : lcfirst($str);
+    }
+
+    /**
+     * Returns an SnakeCase string
+     *
+     * @param bool|false $upper If true the result will be an Screaming Snake Case string
+     * @return string
+     */
+    public function snake($upper = false)
+    {
+        $str = implode('_', $this->words);
+        return $upper ? mb_strtolower($str) : mb_strtoupper($str);
+    }
+
+
+
 
     /**
      * Converts a Camel Case string to Snake Case
@@ -51,7 +82,7 @@ class Convert
      * @param bool|false $upper
      * @return string
      */
-    public static function camelToSnake($str, $upper = false)
+    public static function old_camelToSnake($str, $upper = false)
     {
         $res = preg_replace_callback('|[A-Z]{1,}|', function ($m) {
             return '_' . reset($m);
@@ -67,7 +98,7 @@ class Convert
      * @param bool|false $upper
      * @return string
      */
-    public static function snakeToCamel($str, $upper = false)
+    public static function old_snakeToCamel($str, $upper = false)
     {
         $res = preg_replace_callback('|_{1,}\w|', function ($m) {
             return mb_strtoupper(trim(reset($m), '_'));
