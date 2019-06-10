d676138cc9e52e8cc1f106698e25cf38e1b85fb5
jawira <dev@tugal.be>
2019-06-10 22:18:42 +0200

    Unit test for NamingConvention::glueUsingRules #35

--- tests/phpunit/NamingConventionTest.php
+++ tests/phpunit/NamingConventionTest.php
@@ -1,11 +1,9 @@
 <?php
 
-use Jawira\CaseConverter\Convert;
 use Jawira\CaseConverter\DashBased;
 use Jawira\CaseConverter\NamingConvention;
 use Jawira\CaseConverter\SpaceBased;
 use Jawira\CaseConverter\UnderscoreBased;
-use Jawira\CaseConverter\UppercaseBased;
 use PHPUnit\Framework\TestCase;
 
 class NamingConventionTest extends TestCase
@@ -64,19 +62,19 @@ class NamingConventionTest extends TestCase
      *
      * @param array  $words
      * @param string $glue
-     * @param int    $mode
-     * @param bool   $lowerCaseFirst
+     * @param int    $wordsMode
+     * @param int    $firstWordMode
      *
      * @param string $expected
      *
      * @throws \ReflectionException
      */
-    public function testGlueUsingRules(array $words, string $glue, int $mode, bool $lowerCaseFirst, string $expected)
+    public function testGlueUsingRules(array $words, string $glue, int $wordsMode, $firstWordMode, string $expected)
     {
         // Disabling constructor without stub methods
         $mock = $this->getMockBuilder(NamingConvention::class)
                      ->disableOriginalConstructor()
-                     ->setMethods([])
+                     ->setMethods(['changeWordsCase', 'changeFirstWordCase'])
                      ->getMockForAbstractClass();
 
         // Making "words" property accessible and setting a value
@@ -90,8 +88,21 @@ class NamingConventionTest extends TestCase
         $method     = $reflection->getMethod('glueUsingRules');
         $method->setAccessible(true);
 
+        // Expectations for changeWordsCase
+        $mock->expects($this->once())
+             ->method('changeWordsCase')
+             ->with($this->equalTo($words, $wordsMode))
+             ->will($this->returnValue($words));
+
+        // Only checking that method is called
+        $expectation = ($firstWordMode) ? $this->once() : $this->never();
+        $mock->expects($expectation)
+             ->method('changeFirstWordCase')
+             ->with($words, $firstWordMode)
+             ->will($this->returnValue($words));
+
         // Testing
-        $output = $method->invoke($mock, $glue, $mode, $lowerCaseFirst);
+        $output = $method->invoke($mock, $glue, $wordsMode, $firstWordMode);
         $this->assertSame($expected, $output);
     }
 
@@ -101,36 +112,10 @@ class NamingConventionTest extends TestCase
     public function glueUsingRulesProvider()
     {
         return [
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_LOWER, false, 'foo-bar'],
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_TITLE, false, 'Foo-Bar'],
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_UPPER, false, 'FOO-BAR'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_LOWER, false, 'foo_bar'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_TITLE, false, 'Foo_Bar'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_UPPER, false, 'FOO_BAR'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_LOWER, false, 'foobar'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_TITLE, false, 'FooBar'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_UPPER, false, 'FOOBAR'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_LOWER, false, 'foo bar'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_TITLE, false, 'Foo Bar'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_UPPER, false, 'FOO BAR'],
-            [['foo', 'bar'], '§', MB_CASE_LOWER, false, 'foo§bar'],
-            [['foo', 'bar'], '§', MB_CASE_TITLE, false, 'Foo§Bar'],
-            [['foo', 'bar'], '§', MB_CASE_UPPER, false, 'FOO§BAR'],
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_LOWER, true, 'foo-bar'],
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_TITLE, true, 'foo-Bar'],
-            [['foo', 'bar'], DashBased::DELIMITER, MB_CASE_UPPER, true, 'foo-BAR'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_LOWER, true, 'foo_bar'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_TITLE, true, 'foo_Bar'],
-            [['foo', 'bar'], UnderscoreBased::DELIMITER, MB_CASE_UPPER, true, 'foo_BAR'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_LOWER, true, 'foobar'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_TITLE, true, 'fooBar'],
-            [['foo', 'bar'], UppercaseBased::DELIMITER, MB_CASE_UPPER, true, 'fooBAR'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_LOWER, true, 'foo bar'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_TITLE, true, 'foo Bar'],
-            [['foo', 'bar'], SpaceBased::DELIMITER, MB_CASE_UPPER, true, 'foo BAR'],
-            [['foo', 'bar'], '§', MB_CASE_LOWER, true, 'foo§bar'],
-            [['foo', 'bar'], '§', MB_CASE_TITLE, true, 'foo§Bar'],
-            [['foo', 'bar'], '§', MB_CASE_UPPER, true, 'foo§BAR'],
+            [['fOo', 'bAr'], '§', MB_CASE_LOWER, null, 'fOo§bAr'],
+            [['fOo', 'bAr'], '§', MB_CASE_LOWER, MB_CASE_LOWER, 'fOo§bAr'],
+            [['fOo', 'bAr'], 'X', MB_CASE_LOWER, null, 'fOoXbAr'],
+            [['fOo', 'bAr'], 'X', MB_CASE_LOWER, MB_CASE_LOWER, 'fOoXbAr'],
         ];
     }
 }
