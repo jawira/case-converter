462fca98072463a88d36512482b792b88fbe1f70
jawira <dev@tugal.be>
2019-06-11 22:12:03 +0200

    \UnderscoreSplitterTest #35

---
+++ tests/phpunit/UnderscoreSplitterTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Jawira\CaseConverter\UnderscoreSplitter;
+use PHPUnit\Framework\TestCase;
+
+class UnderscoreSplitterTest extends TestCase
+{
+    /**
+     * @covers \Jawira\CaseConverter\UnderscoreSplitter::split
+     */
+    public function testSplit()
+    {
+        // Disabling constructor with one stub method
+        $mock = $this->getMockBuilder(UnderscoreSplitter::class)
+                     ->disableOriginalConstructor()
+                     ->setMethods(['splitUsingPattern'])
+                     ->getMock();
+
+        // Setting value to protected property
+        $reflectionObject   = new ReflectionObject($mock);
+        $reflectionProperty = $reflectionObject->getProperty('inputString');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($mock, 'dummyString');
+
+        // Configuring stub
+        $mock->expects($this->once())
+             ->method('splitUsingPattern')
+             ->with('dummyString', '_+')
+             ->willReturn(['dummy', 'array']);
+
+        /** @var \Jawira\CaseConverter\UnderscoreSplitter $mock */
+        $returned = $mock->split();
+        $this->assertSame(['dummy', 'array'], $returned);
+    }
+}
